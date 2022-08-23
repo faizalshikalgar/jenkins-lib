58f6cd23363f34729fcd1d9d13785ffbd2a3c2d0
faizalshikalgar <85288970+faizalshikalgar@users.noreply.github.com>
2022-08-23 13:22:27 +0530

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call () {
-    echo "building the application for branch ${JOB_NAME}"
+    echo "building the application for branch ${BUILD_TAG}"
     sh 'mvn package'
 }
