799d534482d39fcd9c1396aa9f8788cc55a578be
faizalshikalgar <faizalshikalgar@gmail.com>
2022-08-23 14:53:50 +0530

    docker class devided

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -13,10 +13,14 @@ class Docker implements Serializable {
 
     def buildDockerImages(String imageName) {
         script.echo "building the docker image and push..."
+        script.sh "docker build -t ${imageName} ."
+    }
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'dockerHub-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t ${imageName} ."
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push ${imageName}"
+        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
+    }
+    def dockerLogin(String imageName) {
+        script.sh "docker push ${imageName}"
     }
 }
