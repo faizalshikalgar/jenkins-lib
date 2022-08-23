f5ca663e1ffbe79b65e070e8667113b943de7a4e
faizalshikalgar <85288970+faizalshikalgar@users.noreply.github.com>
2022-08-23 13:43:27 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String imageName) {
+def call(imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerHub-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ${imageName} .'
